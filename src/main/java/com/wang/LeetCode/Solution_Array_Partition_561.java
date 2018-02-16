32668298de8c6b955fb6ce3479cbd226e3c6e275
joker <342028413@qq.com>
2018-02-17 01:00:58 +0800

    no_decreasing

--- src/main/java/com/wang/LeetCode/Solution_Array_Partition_561.java
+++ src/main/java/com/wang/LeetCode/Solution_Array_Partition_561.java
@@ -13,6 +13,7 @@ import java.util.Collections;
 public class Solution_Array_Partition_561 {
     public int arrayPairSum(int[] nums) {
         int sum = 0;
+        Arrays.sort(nums);
         for(int i=0; i<nums.length; i+=2)
             sum += nums[i];
 
